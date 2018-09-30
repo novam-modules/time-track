de543b130d72f028232ab9f2bab392c25f1d10db
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-30 05:20:11 -0600

    Updated vue components

---
+++ Resources/components/TimeCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="time-card">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "time-card",
+    props: [],
+    data(){
+        return {}
+    }}
+</script>
+
+<style>
+
+</style>
---
+++ Resources/components/TimeClock.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="time-clock">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "time-clock",
+    props: [],
+    data(){
+        return {}
+    }}
+</script>
+
+<style>
+
+</style>
