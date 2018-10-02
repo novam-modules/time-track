9c1b9ae0d6587bf3c60f5bcd1642d65e4087f292
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-01 23:22:31 -0600

    Updted timesheet and index view

--- Resources/components/TimeSheet.vue
+++ Resources/components/TimeSheet.vue
@@ -2,17 +2,26 @@
     <table class="table table-hover">
         <thead class="bg-primary">
             <tr>
+                <th>Date</th>
                 <th>Start At</th>
                 <th>Finish At</th>
                 <th>Clocked In</th>
                 <th>Clocked Out</th>
-                <th>Assignment</th>
+                <th>Project</th>
+                <th>Task/Work Order</th>
                 <th>Total Hours</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="(assigned, index) in assignments" :key="assigned.id || index">
-
+                <td>TBD</td>
+                <td>TBD</td>
+                <td>TBD</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
         </tbody>
     </table>
--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('timetrack::layouts.master')
 
 @section('content')
-    <h1>Time Track</h1>
+    <h1>Time Sheet</h1>
 
     <div id="time-sheet" class="table-responsive">
         <time-sheet :source="'/admin/projects/tasks'"></time-sheet>
